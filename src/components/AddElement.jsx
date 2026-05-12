6cb2835638dce59524f9292ba89004953fc323d6
Giacomo Lanza <giacomo.lanza@ptb.de>
2026-05-12 10:52:45 +0200

    add date

--- src/components/AddElement.jsx
+++ src/components/AddElement.jsx
@@ -36,7 +36,7 @@ const AddElement = ({ enumerated, field_enumerate, field_required, defaultSchema
     let tempUISchema = JSON.parse(JSON.stringify(defaultSchema))
 
 
-    const datatypes = ["string", "number", "integer", "object", "array", "boolean"]
+    const datatypes = ["string", "number", "integer", "object", "array", "boolean", "date"]
 
 
     const handleOnChangeListField = (event) => {
