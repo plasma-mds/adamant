41e90bbf46e0767e0227f9d05e8330bfdb028cf1
Shashank Shekher Tripathi <shekharshashank1@gmail.com>
2026-05-18 11:09:25 +0200

    Changed to HashRouter to resolve deployment issue

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-//import { HashRouter as Router } from "react-router-dom";
+//import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
